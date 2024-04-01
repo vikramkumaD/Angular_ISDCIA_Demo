80f50e0016899f1b5a6172994acd44ad90634cb9
Vikram Kumar Dulgach (ext) <vikram-kumar.dulgach.external@eviden.com>
2024-04-01 14:54:20 +0530

    first commit

---
+++ src/app/pages/splash_screen/splash-screen/splash-screen.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-splash-screen',
+  standalone: true,
+  imports: [],
+  templateUrl: './splash-screen.component.html',
+  styleUrl: './splash-screen.component.scss'
+})
+export class SplashScreenComponent {
+
+}
